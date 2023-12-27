2086a824b13cb6e08fdbe0ec099589934699e530
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-27 10:30:41 -0500

    The goal of this section was to subscribe to the returned observable through the component. This was done by injecting an instance of our ProductService. We then placed our observable in an ngOnInit life cycle hook in order to subscribe to the observable. We then reacted to the emission by adding an observer inside the subscribe method. We then have the products coming in equal to the empty product array in our component to show in the template. Finally, we unsubscribe from the observer so meet good coding practices

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
     selector: 'pm-product-list',
@@ -10,16 +12,41 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
     standalone: true,
   imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
 })
-export class ProductListComponent {
-  // Just enough here for the template to compile
+export class ProductListComponent implements OnInit, OnDestroy {
+  // For the ProductListComponent, we want to load the products when the component is initialized
+  // and we want to unsubscribe from the product observable when the component is destroyed
+  
   pageTitle = 'Products';
   errorMessage = '';
+
+  // We use this property to unsubscribe from our observable (keeps best practice)
+  sub!: Subscription;
+
+  // Inject our ProductService in order to get the product so we can show it in our template
+  private productService = inject(ProductService);
 
   // Products
   products: Product[] = [];
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
+
+  ngOnInit(){
+    this.sub = this.productService.getProducts()
+    .pipe(
+      tap(() => console.log('In component pipeline'))
+    )
+    // In order to react to the emissions when the observable emits our product array we need to 
+    // add an observer inside the subscribe method
+    .subscribe(products => {
+      this.products = products;
+      console.log(this.products);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;

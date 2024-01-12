7ab9b2162cd8e730e51cb7843932c5c57baa7043
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-12 10:27:07 -0500

    In this version the goal was to create a method that can return a product that has reviews and then create a place to call that method from. To start I create the getProductWithReviews method, which takes in a product and emits an observable using the Product interface. Then I create an if block, if the product has the hasReviews property, then we issue an http.get request of type Review array (This is in case there are multiple reviews.). For the get method I pass in the getReviewUrl method from the review service. and pass in the product.id. I pipe this returned array of reviews and use the map operator to further manipulate the data. I pass in the reviews, and then use the spread operator to create a copy of products and append the reviews being passed through the map operator (We use 'as' Product because it tells TS to treat the resulting object as an instance of the Product type). Else, if this is false then we don't need to get reviews, so we return the original product. However, we expect an obserable, so I use the of operator to make the product an observable. In the getProduct method I add the map operaotr just above the catchError operator since might return an error. I pass in the product to getProductWithReviews method, which emits an observable. At the moment we are not subscribed so nothing is happening. And I have an inner observable so we need to change that. This will be done through the higher order mapping operators.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,25 +1,27 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, catchError, of, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  // Acts as our url
+
   private productsUrl = 'api/products';
-  // Inject our HttpErrorService
   private errorService = inject(HttpErrorService)
   private http = inject(HttpClient);
+  // Private so the injected instance can only be used by this service
+  private reviewService = inject(ReviewService)
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
         tap(() => console.log('In http.get pipeline.')),
-        // We replace the multiline error function with the handleError function and pass in error
         catchError(err => this.handleError(err))
       );
   }
@@ -29,19 +31,39 @@ export class ProductService {
     return this.http.get<Product>(productUrl)
       .pipe(
         tap(() => console.log('In http.get by id pipeline')),
+        // We want the map above catchError since it can generate an error
+        map(product => this.getProductWithReviews(product)),  // Pass product to getProductWithReviews method, emits an observable (inner observable) 
         catchError(err => this.handleError(err))
       )
   }
 
+  // Method to getProductWithReviews takes in a product (uses the product to retrieve the reviews)
+  // This method emits an oberservable that emits the updated product with its reviews
+  private getProductWithReviews(product: Product): Observable<Product>{
+    // check if product has hasReviews property
+    if (product.hasReviews){
+      // If so, issue the http get request 
+      // There may be multiple reviews, so we expect to get Review array
+      // For the url we use reviewService.getReviewUrl and pass in the product id
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))  // returns an array of reviews
+        //  We have an inner observable
+        .pipe(
+          // Create a copy of the passed in product using spread operator and append reviews to it
+          // 'as' Product tells TS to treat the resulting object as an instance of the 'Product' type
+          map(reviews => ({ ...product, reviews} as Product))
+        )
+    } else {
+      // If false, then we don't need to get reviews, so return original product
+      // Make sure to return the product as an observable, so we use 'of' operator
+      return of(product);
+    }
+  }
+
+
+
   // Create a private HandleError method
   private handleError(err: HttpErrorResponse): Observable<never>{  // We use never because we want an observable that doesn't emit anything and doesn't complete
-    // We simple want this method to error out
-
-    // Declare a variable for our formatted message, then call the errorService formatError method
     const formattedMessage = this.errorService.formatError(err);
-
-    // Return throwError. ThrowError creates a replacement observable that when subscribe emits an error notification with the error message
-    // We pass in a function that returns a formatted error message
     return throwError(() => formattedMessage);
   }
 }
--- src/app/reviews/review.service.ts
+++ src/app/reviews/review.service.ts
@@ -7,9 +7,10 @@ export class ReviewService {
   // Just enough here for the code to compile
   private reviewsUrl = 'api/reviews';
 
+  // Building the get url endpoint
   getReviewUrl(productId: number): string {
-    // Use appropriate regular expression syntax to
-    // get an exact match on the id
+    // Below we use the base URL, appends a query parameter. We use caret and dollar regular expression anchor to 
+    // get an exact match on the product id
     return this.reviewsUrl + '?productId=^' + productId + '$';
   }
 }

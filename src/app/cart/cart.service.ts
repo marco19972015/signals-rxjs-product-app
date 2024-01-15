596b27c6cdec9c3e8337fe63206ec0626f8814cf
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-15 16:49:00 -0500

    In this version I added 4 new signals that react to prices when a product is added to a cart. From there in the cart-total.component.ts I inject the cartService so the template isn't dependent on the service but instead properties in the compont. From there in the cart-total.component.html I make the changes in the template so that we reference the signals and not the old properties.

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -9,22 +9,21 @@ export class CartService {
   cartItems = signal<CartItem[]>([]);
 
   cartCount = computed(() => this.cartItems()
-    // We call the JS reduce method to accumulate the total quantity of items.
-    // The array reduce method loops through each item in the cart and accumulates a value,
-    // as defined by a provided function. 
-    // The reduce method parameters include the accumulated quantity and each item.
-    // As the reduce method loops through each item, we add the current items quantity to the prior accumulated value.
-    // we set 0 to the last argument to the reduce method to set the initial value for the accumulation to 0.
     .reduce((accQty, item) => accQty + item.quantity, 0)
   );
+
+  subTotal = computed(() => this.cartItems().reduce((accTotal, item) => 
+    accTotal + (item.quantity * item.product.price), 0));
+
+  deliveryFee = computed<number>(() => this.subTotal() < 50 ? 5.99 : 0);
+
+  tax = computed(() => Math.round(this.subTotal() * 10.75)/ 100);
+
+  totalPrice = computed(() => this.subTotal() + this.deliveryFee() + this.tax());
 
   eLength = effect(() => console.log('Cart array length: ', this.cartItems().length));
 
   addToCart(product: Product): void{
-    // Why do we make a cope of the array?
-    // Adding, removing or updating an item directly in the array does not change the signal
-    // To change the value in the signal, we update the signal with a new value
-    // In this example, that new value is a new array 
     this.cartItems.update(items => [...items, { product, quantity: 1} ])
     //                              ^ we create a new array from the old array 
     //                                          ^and make any changes to that copy adding the new cart item
